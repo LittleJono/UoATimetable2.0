82710c189517ac124f37d9b2d995035ae68c3080
Jono Templeton <jono@Jonos-MacBook-Air.local>
2018-08-18 14:21:40 +1200

    added server hosting script

---
+++ back_end/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+var rp = require('request-promise');
+
+app.get('/courses', (req, res) => {
+	var queryString = "";
+	var queryKeys = Object.keys(req.query);
+	for (key in queryKeys) {
+		if (key == 0) {
+			queryString += queryKeys[key] + "=" + req.query[queryKeys[key]];
+		} else {
+			queryString += "&" + queryKeys[key] + "=" + req.query[queryKeys[key]];
+		}	
+	}
+	var options = {
+		uri: 'https://api.auckland.ac.nz/service/courses/v2/courses?' + queryString	
+	};
+	console.log(options);
+	rp(options).then((resp) => {
+		console.log("done")
+		res.send(resp)
+	})
+});
+
+app.get('/classes', (req, res) => {
+	var queryString = "";
+	var queryKeys = Object.keys(req.query);
+	for (key in queryKeys) {
+		if (key == 0) {
+			queryString += queryKeys[key] + "=" + req.query[queryKeys[key]];
+		} else {
+			queryString += "&" + queryKeys[key] + "=" + req.query[queryKeys[key]];
+		}	
+	}
+	var options = {
+		uri: 'https://api.auckland.ac.nz/service/classes/v1/classes?' + queryString	
+	};
+	console.log(options);
+	rp(options).then((resp) => {
+		console.log("done")
+		res.send(resp)
+	})
+});
+
+
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
