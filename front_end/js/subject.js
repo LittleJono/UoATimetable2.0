3554aebb355c44b041e4ba10b08bb6a478ec4c8a
BobbyLiu66 <bobbyliu45@gmail.com>
2018-08-19 10:59:18 +1200

    some update

--- front_end/js/subject.js
+++ front_end/js/subject.js
@@ -1,39 +1,69 @@
-var course_list = [
-    {"key": "111", "value": "An Introduction to Practical Computing"},
-    {"key": "101", "value": "Principles of Programming"},
-    {"key": "105", "value": "Principles of Computer Science"},
-    {"key": "107", "value": "Computer Science Fundamentals"},
-    {"key": "210", "value": "Computer Systems 1"},
-    {"key": "215", "value": "Computer Systems 2"},
-    {"key": "220", "value": "Algorithms and Data Structures"},
-    {"key": "225", "value": "Discrete Structures in Mathematics and Computer Science"},
-    {"key": "230", "value": "Programming Techniques"},
-    {"key": "280", "value": "Introduction to Software Development"},
-    {"key": "313", "value": "Computer Organisation"},
-    {"key": "314", "value": "Modern Data Communications"},
-    {"key": "320", "value": "Applied Algorithmics"},
-    {"key": "335", "value": "Distributed Objects, Services and Programming"},
-    {"key": "340", "value": "Operating Systems"},
-    {"key": "345", "value": "Human Computer Interaction"},
-    {"key": "350", "value": "Mathematical Foundations of Computer Science"},
-    {"key": "351", "value": "Fundamentals of Database Systems"},
-    {"key": "361", "value": "Machine Learning"},
-    {"key": "367", "value": "Artificial Intelligence"},
-    {"key": "369", "value": "Computational Science"},
-    {"key": "373", "value": "Computer Graphics and Image Processing"},
-    {"key": "380", "value": "Undergraduate Project in Computer Science"}];
+var course_list =
+    {
+        "111": "An Introduction to Practical Computing",
+        "101": "Principles of Programming",
+        "105": "Principles of Computer Science",
+        "107": "Computer Science Fundamentals",
+        "210": "Computer Systems 1",
+        "215": "Computer Systems 2",
+        "220": "Algorithms and Data Structures",
+        "225": "Discrete Structures in Mathematics and Computer Science",
+        "230": "Programming Techniques",
+        "280": "Introduction to Software Development",
+        "313": "Computer Organisation",
+        "314": "Modern Data Communications",
+        "320": "Applied Algorithmics",
+        "335": "Distributed Objects, Services and Programming",
+        "340": "Operating Systems",
+        "345": "Human Computer Interaction",
+        "350": "Mathematical Foundations of Computer Science",
+        "351": "Fundamentals of Database Systems",
+        "361": "Machine Learning",
+        "367": "Artificial Intelligence",
+        "369": "Computational Science",
+        "373": "Computer Graphics and Image Processing",
+        "380": "Undergraduate Project in Computer Science"
+    };
 
+//load course list
 $("#subject").change(function () {
-    $.each(course_list, function (i, item) {
+    //loop object
+    for (var key in course_list) {
         $('#course').append($('<option>', {
-            value: item.key,
-            text: item.key + " " + item.value
+            value: key,
+            text: key + " " + course_list[key]
         }));
-    });
+    }
 });
 
+//initial calender
+var calendar = new tui.Calendar(document.getElementById('calendar'), {
+    defaultView: "week",
+    taskView: false,
+    disableDblClick: true,
+    isReadOnly: true,
+    useCreationPopup: false,
+    useDetailPopup: false,
+    template: {
+        timegridDisplayPrimayTime: function (time) {
+            var meridiem = time.hour < 12 ? 'am' : 'pm';
+            if (time.hour <= 12) {
+                return time.hour + ':00 ' + meridiem;
+            } else {
+                twelve_hour = time.hour - 12;
+                return twelve_hour + ':00 ' + meridiem;
+            }
+        },
+        timegridDisplayTime: function (time) {
+            return time.hour + ':' + time.minutes;
+        }
+    }
+});
 
-// const axios = require('axios');
+calendar.setOptions({week: {startDayOfWeek: 1}}, true); //sets the start of the week to always be Monday
+calendar.setOptions({month: {startDayOfWeek: 1}}, true);
+calendar.changeView(calendar.getViewName(), true);
+
 
 // Make a request for a user with a given ID
 
@@ -46,18 +76,19 @@ $("#submit").click(function () {
             var sheduleList = []
             var i = 0
             meetingPatterns.forEach(function (items) {
+                //hard code data to last week
                 var dates = {
-                    "mon": new Date(2018, 8, 20),
-                    "tue": new Date(2018, 8, 21),
-                    "wed": new Date(2018, 8, 22),
-                    "the": new Date(2018, 8, 23),
-                    "fri": new Date(2018, 8, 24),
+                    "mon": new Date(2018, 8, 13),
+                    "tue": new Date(2018, 8, 14),
+                    "wed": new Date(2018, 8, 15),
+                    "thu": new Date(2018, 8, 16),
+                    "fri": new Date(2018, 8, 17),
                 };
                 var week = dates[items.daysOfWeek];
-                // console.log(items.daysOfWeek)
+                //initial calender list object
                 var object = {
                     id: '',
-                    calendarId: '',
+                    calendarId: '1',
                     title: '',
                     category: 'time',
                     dueDateClass: '',
@@ -65,11 +96,11 @@ $("#submit").click(function () {
                     end: ''
                 }
                 object.id = i
-                // object.location = items.location
+                object.location = items.location
                 object.title = course
-                object.start = new Date(week.getFullYear(), week.getMonth()-1, week.getDate(), items.startTime.split(":")[0])
-                object.end = new Date(week.getFullYear(), week.getMonth()-1, week.getDate(), items.endTime.split(":")[0])
-                object.calendarId = i
+                //new Date().getMonth() from 1 to 12 but when set date it accept from 0 to 11
+                object.start = new Date(week.getFullYear(), week.getMonth() - 1, week.getDate(), items.startTime.split(":")[0])
+                object.end = new Date(week.getFullYear(), week.getMonth() - 1, week.getDate(), items.endTime.split(":")[0])
                 object.category = 'time'
                 object.isReadOnly = true
                 sheduleList.push(object)
